17684bdbb50081433757ad32bbe1a5b371ed4958
USER <USER@DESKTOP-JUBU7B2>
2026-05-20 15:20:50 +0630

    Feat: TestConnection

---
+++ MPSPDotNetTraining.EFcoreSample/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(
+            "Data Source=.;Initial Catalog=MyShop;User ID=sa;Password=sasa@123;TrustServerCertificate=True;"
+        );
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>()
+            .Property(p => p.Price)
+            .HasPrecision(10, 2);
+    }
+}
